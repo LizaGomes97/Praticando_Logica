202be1b7f9d9e1825a613301bd2d471b38dc424c
Lizandra <lizandraplacido@gmail.com>
2025-04-12 12:09:04 -0300

    Refatorar função adicionar para obter elementos do carrinho e calcular subtotal

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,41 +1,67 @@
-function adicionar() {
-  let produtoSelecionado = document.querySelector("#produto").value;
+function obterElementos() {
+  let valorUnitario = document.querySelector("#produto").value.split("$")[1];
+  let valorUnitarioInt = parseInt(valorUnitario);
   let quantidadeProduto = document.querySelector("#quantidade").value;
-  let carrinho = document.querySelector("#lista-produtos");
-  let valorTotalElemento = document.querySelector("#valor-total");
 
-  //condiçao que adiciona um se a quantidade nao foi definida
-  if (quantidadeProduto == "") {
+  if (quantidadeProduto === "") {
     quantidadeProduto = 1;
+  } else {
+    quantidadeProduto = parseInt(quantidadeProduto);
   }
 
-  quantidadeProduto = parseInt(quantidadeProduto);
-
-  let detalhes = definirProdutoAndPreco(produtoSelecionado);
-  let produto = detalhes.produto;
-  let preco = detalhes.preco;
-
-  criarItemNovoNoCarrinho(carrinho, quantidadeProduto, produto, preco);
-
-  let valorAtualTexto = valorTotalElemento.textContent;
-
-  let novoValorTotal = aumentarTotal(valorAtualTexto, preco, quantidadeProduto);
-
-  valorTotalElemento.textContent = `R$${novoValorTotal}`;
+  return {
+    produtoSelecionado: document.querySelector("#produto").value,
+    nomeProduto: document.querySelector("#produto").value.split("-")[0],
+    quantidadeProduto,
+    subtotalCarrinho: valorUnitarioInt * quantidadeProduto,
+    produtosCarrinho: document.querySelector("#lista-produtos"),
+    valorTotalElemento: document.querySelector("#valor-total"),
+    valorTotalCarrinho: document.querySelector("#valor-total").textContent,
+  };
 }
 
-function separarProdutoDoPreco(string) {
-  return string.split(" ");
+function adicionar() {
+  const elementos = obterElementos();
+
+  elementos.quantidadeProduto = parseInt(elementos.quantidadeProduto);
+  console.log(
+    "Quantidade de produto apos o a variavel:",
+    elementos.quantidadeProduto
+  );
+
+  criarItemNovoNoCarrinho(
+    elementos.produtosCarrinho,
+    elementos.quantidadeProduto,
+    elementos.nomeProduto,
+    elementos.subtotalCarrinho
+  );
+
+  let novoValorTotal = aumentarTotal(
+    elementos.valorTotalCarrinho,
+    elementos.subtotalCarrinho,
+    elementos.quantidadeProduto
+  );
+
+  elementos.valorTotalElemento.textContent = `R$${novoValorTotal}`;
 }
 
-function aumentarTotal(valorTotalTexto, precoStr, quantidade) {
-  valorDoItem = parseInt(precoStr.replace(/[^\d]/g, ""));
-  valorDoCarrinho = parseInt(valorTotalTexto.replace(/[^\d]/g, ""));
+function aumentarTotal(valorTotalTexto, subtotal, quantidade) {
+  // console.log("Valor do carrinho no inicio da funçao:", valorDoCarrinho);
+  let valorDoItem = subtotal;
+
+  let valorDoCarrinho;
+  if (valorTotalTexto.includes("R$")) {
+    valorDoCarrinho = parseInt(valorTotalTexto.split("R$")[1]);
+  } else {
+    valorDoCarrinho = parseInt(valorTotalTexto);
+  }
 
   console.log("Valor do item:", valorDoItem);
+  console.log("Quantidade:", quantidade);
   console.log("Valor atual do carrinho:", valorDoCarrinho);
 
-  let novoValorTotal = valorDoItem * quantidade + valorDoCarrinho;
+  let novoValorTotal = valorDoItem + valorDoCarrinho;
+  console.log("novoValorTotal:", novoValorTotal);
 
   return novoValorTotal;
 }
@@ -45,21 +71,10 @@ function criarItemNovoNoCarrinho(carrinho, quantidadeProduto, produto, preco) {
   let novoItem = document.createElement("section");
   novoItem.className = "carrinho__produtos__produto";
   novoItem.innerHTML = `
-    <span class="texto-azul">${quantidadeProduto}X</span> ${produto} <span class="texto-azul">${preco}</span>
+    <span class="texto-azul">${quantidadeProduto}X</span> ${produto} <span class="texto-azul">R$${preco}</span>
     `;
 
   carrinho.appendChild(novoItem);
-}
-
-function definirProdutoAndPreco(produtoSelecionado) {
-  let lista = separarProdutoDoPreco(produtoSelecionado);
-  let produto = lista[0];
-  let preco = lista[lista.length - 1];
-
-  return {
-    produto: produto,
-    preco: preco,
-  };
 }
 
 function limpar() {
